9820af2eae69914f7b8ead247f2c55ccb2813ab8
Wen Du <duwdora@gmail.com>
2020-04-07 14:19:33 -0700

    update loadAudio.js

--- loadAudio.js
+++ loadAudio.js
@@ -7,16 +7,9 @@ const Max = require('max-api');
 // This will be printed directly to the Max console
 Max.post(`Loaded the ${path.basename(__filename)} script`);
 
-let currentDir = process.cwd();
-let samples = `${currentDir}/samples`;
+// let currentDir = process.cwd();
+let sampleDir = '';
 let filenames = [];
-fs.readdir(samples, (err, files) => {
-	if (err) throw err;
-
-	for (let index in files) {
-		filenames.push(files[index]);
-	}
-})
 
 // Use the 'addHandler' function to register a function for a particular message
 Max.addHandler('readWave', (fileIndex) => {
@@ -24,7 +17,29 @@ Max.addHandler('readWave', (fileIndex) => {
 	getWaveFile(fileIndex);
 });
 
-function getWaveFile(pos) {	
+Max.addHandler('setFileDir', (inputDir) => {
+	setFileDir(inputDir);
+	extractFileNames();
+});
+
+function setFileDir(fileDir) {
+	Max.post(`sample file directory is ${fileDir}`);
+	sampleDir = fileDir;
+}
+
+function extractFileNames() {
+	fs.readdir(sampleDir, (err, files) => {
+		if (err) {
+			Max.post(err);
+		} else {
+			for (let index in files) {
+				filenames.push(files[index]);
+			}
+		}
+	});
+}
+
+function getWaveFile(pos) {
 	Max.post(filenames[pos]);
-	Max.outlet(filenames[pos]);
+	Max.outlet(`${sampleDir}/${filenames[pos]}`);
 }
